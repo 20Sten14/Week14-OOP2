3e6163ce5ef208016ab79bb0416666f092b7ca71
Opilane TTHK <opilane@tthk.ee>
2021-12-01 13:07:13 +0200

    OOP2

---
+++ OOPCar/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace OOP_Part1
+{
+    class Program
+    {
+        class Car
+        {
+            string mark;
+            string model;
+            string regNum;
+            string color;
+            int odometer;
+            int fuel;
+
+            public Car(string _mark, string _model, string _regNum, string _color)
+            {
+                mark = _mark;
+                model = _model;
+                regNum = _regNum;
+                color = _color;
+                odometer = 0;
+                fuel = 20;
+
+                Console.WriteLine($"you've made your {_mark} and model {_model}");
+            }
+            /*public double Weight
+            {
+                get { return w; }
+            }*/
+            public void ShowCarInfo()
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Cars name: {mark}");
+                Console.WriteLine($"Cars Model:{model}");
+                Console.WriteLine($"Cars regNum: {regNum}");
+                Console.WriteLine($"Cars color: {color}");
+                Console.WriteLine($"Car's odometer: {odometer}");
+                Console.WriteLine($"Car's fuel level: {fuel}");
+
+            }
+            public string Model
+            {
+                get { return model; }
+            }
+            public string Mark
+            {
+                get { return mark; } 
+            }
+            public string RegNum
+            {
+                get { return regNum; }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+            public int Odometer
+            {
+                get { return odometer; }
+            }
+            public int Fuel
+            {
+                get { return fuel; }
+            }
+            public void drive()
+            {
+                fuel -= 5;
+                odometer += 80;
+                Console.WriteLine("Zoom zoom");
+            }
+           static void Main(string[] args)
+           {
+                Car newcar = new Car("Subaru", "Legacy", " 871BJH", "Black");
+                
+                while (newcar.Fuel > 0) 
+                {
+                    
+                  newcar.drive();
+                  Console.WriteLine($"Fuel after drive: {newcar.Fuel}");
+                    
+                }
+                newcar.ShowCarInfo();
+           }
+
+
+
+        }
+    }
+}
---
+++ ObjectsFromFile/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace ObjectsFromFile
+{
+    class Program
+    {
+        class Movie
+        {
+            public string title;
+            public string rating;
+            public string year;
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+
+            static void Main(string[] args)
+            {
+                foreach (string movie in GetDataFromFile()) 
+                {
+
+                    string[] temarrays = movie.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    Movie NewMovie = new Movie(temarrays[0], temarrays[1], temarrays[2]);
+
+                    Console.WriteLine($"Title: {NewMovie.title},  rating {NewMovie.rating}, year: {NewMovie.year}");
+                }
+                Console.WriteLine($"Title:, rating:, year:");
+            }
+            public static string[] GetDataFromFile()
+            {
+                string filepath = @"C:\Users\opilane\samples\Movies.txt";
+                string[] datafromfile = File.ReadAllLines(filepath);
+                return datafromfile;
+            }
+            public static void DisplayArraysElements(string[] someArrays)
+            {
+                foreach (string element in someArrays)
+                {
+                    Console.WriteLine(element);
+
+                }
+
+            }
+        }
+    }
+}
